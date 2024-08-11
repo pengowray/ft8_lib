681807a583d28680691706e87fd5e35a8e9c187c
Pengo Wray <peter@becauseofgames.com>
2024-08-12 00:27:53 +1000

    annotations improved for output display; grid square to lat lon

--- ft8_wasm/web/ft8_anno.js
+++ ft8_wasm/web/ft8_anno.js
@@ -1,5 +1,5 @@
-const def_i3n3 = { label: "Type", tag:'i3.n3', start: 71, length: 6, getValue: (bits) => bitsToi3n3(bits) };
-const def_i3 = { label: "Type", shortLabel:'i3', tag:'i3', start: 74, length: 3, getValue: (bits) => bitsToi3(bits) };
+const def_i3n3 = { label: "Type", tag:'i3.n3', start: 71, length: 6, getValue: bitsToi3n3 };
+const def_i3 = { label: "Type", shortLabel:'i3', tag:'i3', start: 74, length: 3, getValue: bitsToi3 };
 
 // note: don't use shortLabel if not needed to display on tribble bit display
 
@@ -9,84 +9,97 @@ const annotationDefinitions = {
         def_i3n3
     ],
     "0.1": [
-        { label: "Call1", tag:'c28', start: 0, length: 28, getValue: bitsToCall},
+        { label: "Call1", tag:'c28', start: 0, length: 28, getValue: bitsToCall },
         { label: "Call2", tag:'c28', start: 28, length: 28, getValue: bitsToCall },
         { label: "Hash", tag:'h10', start: 56, length: 10, getValue: bitsToHash },
-        { label: "Report", tag:'r5', start: 66, length: 5, getValue: bitsToReport },
+        { label: "Report",  tag:'r5', start: 66, length: 5, getValue: bitsToReport },
         def_i3n3
     ],
-    "0.2": [ // EU VHF Contest (is this real?)
-        { label: "Call1", tag:'c28', start: 0, length: 28, getValue: bitsToCall},
+    "0.2": [ //(is this anything?)
+        def_i3n3
+    ],
+    "0.3": [ // ARRL Field Day
+        { label: "Call1", tag:'c28', start: 0, length: 28, getValue: bitsToCall },
         { label: "Call2", tag:'c28', start: 28, length: 28, getValue: bitsToCall },
-        { label: "R", start: 56, length: 1, getValue: (bit) => bit === '1' ? 'R' : '' },
-        { label: "Serial", start: 57, length: 13, getValue: (bits) => placeholder(bits) },
-        def_i3n3
-    ],
-    "0.3": [ // ARRL Field Day c28 c28 R1 n4 k3 S7
-        { label: "Call1", start: 0, length: 28, getValue: (bits) => bitsToCall(bits) },
-        { label: "Call2", start: 28, length: 28, getValue: (bits) => bitsToCall(bits) },
         { label: "/R", shortLabel: 'R', tag:'R1', start: 56, length: 1, getValue: (bit) => bit === '1' ? 'R' : '' },
-        { label: "Number of transmitters", shortLabel: 'nTX', tag:'n4', start: 57, length: 4, getValue: (bits) => bitsToTxNumber(bits) },
-        { label: "Class", tag:'', start: 61, length: 3, getValue: (bits) => bitsToFieldDayClass(bits) },
-        { label: "Section", start: 64, length: 7, getValue: (bits) => bitsToARRLSection(bits) },
+        { label: "Number of transmitters", tag:'n4', shortLabel: 'nTX', tag:'n4', start: 57, length: 4, getValue: bitsToTxDetails },
+        { label: "Class", tag:'k3', start: 61, length: 3, getValue: bitsToFieldDayClass },
+        { label: "Section",tag: 'S7', start: 64, length: 7, getValue: bitsToARRLSection },
         def_i3n3
     ],
-    "0.4": [ // ARRL Field Day (alternate format) c28 c28 R1 n4 k3 S7
+    "0.4": [ // ARRL Field Day (alternate format)
         //n4 Number of transmitters: 1-16, 17-32
         //S7 ARRL/RAC Section
-        { label: "Call1", tag:'c28', start: 0, length: 28, getValue: (bits) => bitsToCall(bits) },
-        { label: "Call2", tag:'c28', start: 28, length: 28, getValue: (bits) => bitsToCall(bits) },
+        { label: "Call1", tag:'c28', start: 0, length: 28, getValue: bitsToCall },
+        { label: "Call2", tag:'c28', start: 28, length: 28, getValue: bitsToCall },
         { label: "R", tag:'R1', start: 56, length: 1, getValue: (bit) => bit === '1' ? 'R' : '' },
-        { label: "Number of transmitters", shortLabel: 'nTX', tag:'n4', start: 57, length: 4, getValue: (bits) => bitsToTxNumber(bits) },
-        { label: "Class", start: 61, length: 3, getValue: (bits) => bitsToFieldDayClass(bits) },
-        { label: "Section", start: 64, length: 7, getValue: (bits) => bitsToARRLSection(bits) },
+        { label: "Number of transmitters", shortLabel: 'nTX', tag:'n4', start: 57, length: 4, getValue: bitsToTxDetails },
+        { label: "Class",  tag:'k3', start: 61, length: 3, getValue: (bits) => bitsToFieldDayClass(bits) },
+        { label: "Section", tag: 'S7', start: 64, length: 7, getValue: (bits) => bitsToARRLSection(bits) },
         def_i3n3
     ],
     "0.5": [ // Telemetry
-        { label: "Telemetry", start: 0, length: 71, getValue: (bits) => telemetryBitsToText(bits) },
+        { label: "Telemetry", tag: 't71', start: 0, length: 71, getValue: (bits) => telemetryBitsToText(bits) },
         def_i3n3
     ],
-    "1": [ // Standard message c28 r1 c28 r1 R1 g15
-        { label: "Call1", tag:'c28', start: 0, length: 28, getValue: (bits) => bitsToCall(bits) },
-        { label: "/R for Call1", shortLabel:'r', tag:'r1', start: 28, length: 1, getValue: (bit) => bit === '1' ? '/R' : '' },
-        { label: "Call2", start: 29, length: 28, getValue: (bits) => bitsToCall(bits) },
-        { label: "/R for Call2", shortLabel:'r',  tag:'r1', start: 57, length: 1, getValue: (bit) => bit === '1' ? '/R' : '' },
-        { shortLabel: 'R', tag:'R1', start: 58, length: 1, getValue: (bit) => bit === '1' ? 'R' : '' },
-        { label: "Grid/Report", tag:'g15', start: 59, length: 15, getValue: (bits) => bitsToGrid4OrReportWithType(bits) },
+    "0.6": [ // undefined
+        def_i3n3
+    ],
+    "0.7": [ // undefined
+        def_i3n3
+    ],
+
+    "1": [ // Standard message
+        { label: "Call1", tag:'c28', start: 0, length: 28, getValue: bitsToCall },
+        { label: "/R for Call1", shortLabel:'r', tag:'r1', start: 28, length: 1, getValue: (bit) => bit === '1' ? '1 /R' : '0' },
+        { label: "Call2", tag:'c28', start: 29, length: 28, getValue: bitsToCall },
+        { label: "/R for Call2", shortLabel:'r',  tag:'r1', start: 57, length: 1, getValue: (bit) => bit === '1' ? '1 /R' : '0' },
+        { shortLabel: 'R', tag:'R1', start: 58, length: 1, getValue: (bit) => bit === '1' ? '1 (R)' : '0' },
+        { label: "Grid/Report", tag:'g15', start: 59, length: 15, getValue: bitsToGrid4OrReportWithType },
         def_i3
     ],
     "2": [ // EU VHF Contest c28 p1 c28 p1 R1 g15
-        { label: "Call1", start: 0, length: 28, getValue: (bits) => bitsToCall(bits) },
+        { label: "Call1", tag:'c28', start: 0, length: 28, getValue: (bits) => bitsToCall(bits) },
         { label: "/P for Call1", shortLabel:'p', tag:'p1', start: 28, length: 1, getValue: (bit) => bit === '1' ? '/P' : '' },
-        { label: "Call2", start: 29, length: 28, getValue: (bits) => bitsToCall(bits) },
+        { label: "Call2", tag:'c28', start: 29, length: 28, getValue: (bits) => bitsToCall(bits) },
         { label: "/P for Call1", shortLabel:'p', tag:'p1', start: 57, length: 1, getValue: (bit) => bit === '1' ? '/P' : '' },
         { shortLabel: 'R', tag:'R1', start: 58, length: 1, getValue: (bit) => bit === '1' ? 'R' : '' },
-        { label: "Grid4", start: 59, length: 15, getValue: (bits) => bitsToGrid4OrReportWithType(bits) },
+        { label: "Grid4", tag:'g15', start: 59, length: 15, getValue: (bits) => bitsToGrid4OrReportWithType(bits) },
         def_i3
     ],
-    "3": [ // ARRL RTTY Roundup
-        { label: "Call1", start: 0, length: 28, getValue: (bits) => bitsToCall(bits) },
-        { label: "Call2", start: 28, length: 28, getValue: (bits) => bitsToCall(bits) },
-        { label: "R", start: 56, length: 1, getValue: (bit) => bit === '1' ? 'R' : '' },
-        { label: "RST", start: 57, length: 3, getValue: (bits) => bitsToRST(bits) },
-        { label: "Serial/State", start: 60, length: 14, getValue: (bits) => bitsToSerialOrState(bits) },
+    "3": [ // ARRL RTTY Roundup t1 c28 c28 R1 r3 s13
+        { label: "TU", shortLabel:'T', tag:'t1', start: 0, length: 1, getValue: (bit) => bit === '1' ? '1 TU' : '0' },
+        { label: "Call1", tag:'c28', start: 1, length: 28, getValue: (bits) => bitsToCall(bits) },
+        { label: "Call2", tag:'c28', start: 29, length: 28, getValue: (bits) => bitsToCall(bits) },
+        { label: "R", tag:'R1', start: 57, length: 1, getValue: (bit) => bit === '1' ? 'R' : '' },
+        { label: "Report", shortLabel:"RST", tag:'r3', start: 58, length: 3, getValue: bitsToRST },
+        { label: "Serial/State", tag:'s13', start: 61, length: 13, getValue: bitsToSerialOrState },
         def_i3
     ],
     "4": [ // Non-standard call: h12 c58 h1 r2 c1
-        { label: "Hash", start: 0, length: 12, getValue: (bits) => bitsToHash(bits) },
-        { label: "Nonstandard Call", start: 12, length: 58, getValue: (bits) => bitsToNonstandardCall(bits) },
-        { label: "h", start: 70, length: 1, getValue: (bit) => bit === '1' ? '1 (Hash is second callsign)' : '0 (Hash is first callsign)' },
-        { label: "r2", start: 71, length: 2, getValue: (bits) => bitsToR2(bits) },
-        { label: "c", start: 73, length: 1, getValue: (bit) => bit === '1' ? '1 (First callsign is CQ. Ignore hash)' : '0' },
+        { label: "hash", tag:'h12', start: 0, length: 12, getValue: bitsToHash },
+        { label: "Nonstandard call", tag:'c58', start: 12, length: 58, getValue: bitsToNonstandardCall },
+        { label: "Call order", shortLabel: "h", tag: 'h1', start: 70, length: 1, getValue: (bit) => bit === '1' ? '1 (Hash is second callsign)' : '0 (Hash is first callsign)' },
+        { label: "Signoff", tag:'r2', start: 71, length: 2, getValue: bitsToR2 },
+        { label: "CQ", tag:'c1', start: 73, length: 1, getValue: (bit) => bit === '1' ? '1 (First callsign is CQ. Ignore hash)' : '0' },
         def_i3
     ],
-    "5": [ // EU VHF Contest with 6-digit grid locator
-        { label: "Call1", start: 0, length: 28, getValue: (bits) => bitsToCall(bits) },
-        { label: "Call2", start: 28, length: 28, getValue: (bits) => bitsToCall(bits) },
-        { label: "R", start: 56, length: 1, getValue: (bit) => bit === '1' ? 'R' : '' },
-        { label: "Grid6", start: 57, length: 17, getValue: (bits) => bitsToGrid6(bits) },
+    "5": [ // EU VHF Contest with 6-digit grid locator h12 h22 R1 r3 s11 g25
+        { label: "Call1", tag:'h12', start: 0, length: 28, getValue: (bits) => bitsToCall(bits) },
+        { label: "Call2", tag:'h22', start: 28, length: 28, getValue: (bits) => bitsToCall(bits) },
+        { label: "R", tag:'R1', start: 56, length: 1, getValue: (bit) => bit === '1' ? 'R' : '' },
+        { label: "Report", shortLabel:"RST", tag:'r3', start: 57, length: 3, getValue: bitsToRST },
+        { label: "Serial", tag:'s11', desc: 'Serial number (0-2047)', start: 60, length: 13, getValue: bitsToSerialOrState },
+        { label: "Grid6", tag:'g25', start: 57, length: 25, getValue: bitsToGrid6 },
+        def_i3
+    ],
+    "6": [ // undefined
+        def_i3
+    ],
+    "7": [ // undefined
         def_i3
     ]
+
 };
 
 function AnnotationDefGetValueText(annotation, payloadBits77) {
@@ -113,18 +126,20 @@ function AnnotationDefGetAnnotation(annotation, payloadBits77) {
 function bitsToi3n3(bits) {
     if (bits.length !== 6) return placeholder(bits);
 
-    const type = parseInt(bits.slice(0, 3), 2);
-    const n3 = parseInt(bits.slice(3, 6), 2);
-    const short = `${type}.${n3}`;
-    const long = `${short} (${getFT8MessageTypeName(short)})`;
-    return {short, long};
+    const n3 = parseInt(bits.slice(0, 3), 2);
+    const i3 = parseInt(bits.slice(3, 6), 2);
+    const short = `${i3}.${n3}`;
+    const subtype = getFT8MessageTypeName(short);
+    const long = `${short} (${subtype})`;
+    return {short, value:short, long, subtype};
 }
 function bitsToi3(bits) {
     if (bits.length !== 3) return placeholder(bits);
 
-    const short = parseInt(bits.slice(0, 3), 2).toString().toString();
-    const long = `${short} (${getFT8MessageTypeName(short)})`;
-    return {short, long};
+    const short = parseInt(bits, 2).toString();
+    const subtype = getFT8MessageTypeName(short);
+    const long = `${short} (${subtype})`;
+    return {short, value:short, long, subtype};
 }
 
 
@@ -132,10 +147,10 @@ function bitsToi3(bits) {
 function placeholder(bits) {
     const veryShort = parseInt(bits, 2).toString();
     const long = `${veryShort} (undefined value)`;
-    const short = short + "*";
+    const short = veryShort + "*";
     const desc = 'No definition available for this data type. Integer value is shown.';
 
-    return {short, long, veryShort, desc};
+    return {short, long, veryShort, desc, value: veryShort, units: '*', subtype: 'Not yet supported field type'};
 }
 
 //Placeholder for TODOs
@@ -152,7 +167,8 @@ function bitsToGrid6(bits) {
 
 function bitsToHash(bits) {
     //return hashBitsPrettyHex(bits);
-    return hashBitsPrettyZ32(bits);
+    const hashed = hashBitsPrettyZ32(bits);
+    return {value: hashed, subtype:'hash' + bits.length, desc: 'Displayed in Z-Base32 encoding.'};
 }
 
 //TODO
--- ft8_wasm/web/views_output.js
+++ ft8_wasm/web/views_output.js
@@ -78,16 +78,19 @@ class OutputComponent extends Component {
 
             if (normalizeMessage(decoded) !== normalizeMessage(originalInput)) {
                 decodeTest2.resultInfo = "Decoded message does not appear to match input.";
-                decodeTest2.result = 'warning';
-                decodeTest2.resultText = 'might not match input';
+                decodeTest2.result = 'error';
+                decodeTest2.resultText = 'does not match input';
                 if (normalizeMessageAndHashes(originalInput).startsWith(normalizeMessageAndHashes(decoded))) {
                     //todo: only catches if both contain a hash; check if decoded has transformed text into hash
                     //decodeTest2.resultInfo = "Hashes appear different.";
                     decodeTest2.resultText = 'match but unable to check hash data';
+                    decodeTest2.result = 'warning';
 
                 } else if (normalizeBracketedFreeText(originalInput).toUpperCase().startsWith(decoded.toUpperCase())) {
                     decodeTest2.resultInfo = "Original message appears to be truncated.";
                     decodeTest2.resultText = 'input truncated';
+                    decodeTest2.result = 'error';  // warn for plain text, but error for other messages
+                    //decodeTest2.result = 'warning';
                 } 
             } else  {
                 decodeTest2.resultText = 'ok'
@@ -99,11 +102,12 @@ class OutputComponent extends Component {
 
             if (decoded.toUpperCase() !== normalizeBracketedFreeText(originalInput).toUpperCase()) {
                 decodeTest2.resultInfo = "Decoded message does not appear to match free text input.";
-                decodeTest2.result = 'warning';
-                decodeTest2.resultText = 'might not match input';
+                decodeTest2.resultText = 'does not match input';
+                decodeTest2.result = 'error';
                 if (normalizeBracketedFreeText(originalInput).toUpperCase().startsWith(decoded.toUpperCase())) {
                     decodeTest2.resultInfo = "Original message has been truncated to fit 13 character limit of free text.";
                     decodeTest2.resultText = 'input truncated';
+                    decodeTest2.result = 'warning';
                 }
             } else {
                 decodeTest2.resultText = 'ok'
@@ -231,7 +235,7 @@ class OutputComponent extends Component {
                 ${this.renderChecks('Decode check', data.decoded )}
                 ${this.renderRowData('Input text', data.inputText )}
                 ${this.renderRowData('Decoded text', data.decodedText, data.decodedText.includes('<...>') ? '<...> represents a hashed callsign.' : null)}
-                ${!data.decoded[0].success ? this.renderRowData('Decode error', data.decoded[0].errorMessage, "See also the binary visualization chart for more of the message content.") : ''}
+                ${!data.decoded[0].success ? this.renderRowData('Decode error', data.decoded[0].errorMessage, "Please check if individual message fields were were decoded.") : ''}
 
                 ${(data.explanation || data.encodeError) ? this.renderSubheading('More info') : ''}
                 ${data.explanation ? this.renderRowText('Explanation', data.explanation) : ''}
@@ -278,17 +282,17 @@ class OutputComponent extends Component {
                 const label = annotation.tag ?? annotation.label ?? annotation.shortLabel;
                 //TODO: move tag to own field
                 //if (annotation.tag != null && label != annotation.tag) label += ` (${annotation.tag})`;
-                const shortLabel = (annotation.tag != null) ? annotation.label ?? annotation.shortLabel : null;
+                const secondaryLabel = (annotation.tag != null) ? annotation.label ?? annotation.shortLabel : null;
 
                 const pos = annotation.bits.length === 1 ?
                       `bit ${annotation.start + 1}`
                     : `bits ${annotation.start + 1} to ${annotation.start + annotation.length} (length: ${annotation.length} bits)`;
                 
-                const text = annotation.long ?? annotation.value ?? annotation.short;
+                const text = annotation.value ?? annotation.long ?? annotation.short;
                 const note = `Raw bits (=integer): ${annotation.bits} (=${annotation.rawIntValue})\nPosition in payload: ${pos}`;
         
                 //rowContent += this.renderRowData(label, text, note);
-                rowContent += this.renderRowDataField( {...annotation, value: text, label, shortLabel, comment: 'comment example'} );
+                rowContent += this.renderRowDataField( {...annotation, value: text, label, secondaryLabel, comment: 'comment example'} );
             });
         } else {
             console.warn(`No annotation definition for message type: ${ft8MessageType}`);
@@ -305,44 +309,36 @@ class OutputComponent extends Component {
             </div>
         `;
     }
-    
     renderRowDataField(fieldData) {
         const {
             label,
-            shortLabel,
+            secondaryLabel,
             tag,
             value,
-            description,
-            subType,
+            desc,
+            subtype,
             bits,
             rawIntValue,
             start,
             length,
-            comment
+            comment,
+            units
         } = fieldData;
     
         const positionInfo = `bits ${start + 1} to ${start + length} (length: ${length} bits)`;
     
         return `
-            <div class="output-row-field">
-                <div class="output-label-field">
-                    ${escapeHTML(label)}
-                    ${tag ? `<span class="output-sublabel">${escapeHTML(tag)}</span>` : ''}
-                </div>
-                ${shortLabel ? `<div class="output-sublabel">${escapeHTML(shortLabel)}</div>` : ''}
-                <div class="output-value-field">${escapeHTML(value)}</div>
-                ${description ? `<div class="output-description">${escapeHTML(description)}</div>` : ''}
-                ${subType ? `<div class="output-description">${escapeHTML(subType)}</div>` : ''}
-                <div class="output-metadata">
-                    <span class="output-metadata-item">Raw bits: ${escapeHTML(bits)}</span>
-                    <span class="output-metadata-item">Integer value: ${rawIntValue}</span>
-                    <span class="output-metadata-item">Position: ${positionInfo}</span>
-                </div>
-                ${comment ? `<div class="output-comment-field">${escapeHTML(comment)}</div>` : ''}
+            <div class="output-row">
+                <div class="output-label">${secondaryLabel} ${label ? `<span class="output-sublabel">${label}` : ''}</span></div>
+                <div class="output-value"><span class="output-data">${escapeHTML(value)}</span>${ units ? `<span class="output-comment-info"> ${units}</span>` : '' }${ subtype || desc ? `<div class="output-metadata">${subtype}</div>` : ''}${(bits.length >= 2) ? `<div class="output-raw-values">Raw value: ${bits} (=${rawIntValue})</div>` : '' }</div>
+                <div class="output-comment">
+                        ${escapeHTML(desc).replace('\n','<br>')}
+                    </div>
             </div>
         `;
-    }
 
+    }
+    
     renderRowDataHighlights(label, value, highlightIndices = [], ifHighlightsComment = null, colorOverride = null) {
         let highlightedValue = value;
         //Array.isArray(highlightIndices)
@@ -491,7 +487,6 @@ class OutputComponent extends Component {
             let prettyIndex = index;
             if (index >= 72) prettyIndex += 4; 
             else if (index >= 36) prettyIndex += 2; 
-            //prettyIndex += 7;
             
             return prettyIndex;
         });
@@ -540,4 +535,3 @@ function escapeHTML(text) {
         return map[match];
     });
 }
-

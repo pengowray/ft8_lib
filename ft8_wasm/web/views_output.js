2a21f4e08d35c394404159954093abe2350d8822
Pengo Wray <peter@becauseofgames.com>
2024-08-27 20:47:33 +1000

    update flag display

--- ft8_wasm/web/views_output.js
+++ ft8_wasm/web/views_output.js
@@ -330,6 +330,9 @@ class OutputComponent extends Component {
             country,
             rawAppend,
             isField = true, // Field within the data. If rendering something else e.g "Input type", set to false (for smaller font), or lie and say true to make something else big like "Decoded text"
+            isFlag,
+            on,
+            off,
 
         } = fieldData;
     
@@ -346,7 +349,10 @@ class OutputComponent extends Component {
             valueText = `${ callsign ? `<span class="output-call gravity-high"><span class="${ !isHash ? 'call-highlighter':''}">${escapeHTML(callsign)}</span>${operatingStatusIndicator ?? ''} </span>` : '' }<span class="output-hash ${ isHash ? 'gravity-medium':'gravity-low'}" ${(hashIntStr && hashIntStr != 0) ? `title="${hashIntStr}"` : ''}>${addUnderlineToHash(hashBits, isHash ? hashLen : 0)}</span>`
             if (country) { valueText += `<div class="output-country gravity-low">${escapeHTML(country)}</div>`; }
         } else {
-            if (bits == '0' || bits == '1') {
+            if (isFlag) { 
+                valueText = `${this.makeSwitch(bits === '1', off, on)}`;
+
+            } else if (bits == '0' || bits == '1') {
                 //valueText = `<span class="output-data>${this.makeSwitch(bits === '1', '0', '1')}</span>`;
                 valueText = `${this.makeSwitch(bits === '1', '0', '1')}`;
             } else {
@@ -551,12 +557,12 @@ class OutputComponent extends Component {
     makeSwitch(isOn, OffLabel = '0', OnLabel = '1', isDisabled = true) {
         return `
             <div class="toggle-switch toggle-switch${isDisabled ? '--disabled' : ''}">
-                <span class="toggle-switch__label">${OffLabel}</span>
+                <span class="toggle-switch__label ${!isOn ? 'gravity-high' : 'gravity-low'}">${OffLabel}</span>
                 <label class="toggle-switch__track">
                     <input type="checkbox" class="toggle-switch__input" ${isOn ? 'checked' : ''} disabled>
                     <span class="toggle-switch__slider"></span>
                 </label>
-                <span class="toggle-switch__label">${OnLabel}</span>
+                <span class="toggle-switch__label ${isOn ? 'gravity-high' : 'gravity-low'}">${OnLabel}</span>
             </div>
         `;
     }
